6ceab03e424b74d585870e6807481b33fd683b3e
dimat <dimaprizma2016@gmail.com>
2021-01-12 09:07:14 +0200

    Homework 11. Relations. - corrected DateTimeFiels - added migrations

--- catalog/models.py
+++ catalog/models.py
@@ -147,7 +147,7 @@ class LogModel(models.Model):
     """Model representing an author."""
     path = models.CharField(max_length=100)
     method = models.CharField(max_length=100)
-    timestamp = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         """String for representing the Model object."""
--- orders/models.py
+++ orders/models.py
@@ -18,7 +18,7 @@ class Product(models.Model):
     """
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    date_created = models.DateTimeField(auto_now=True)
+    date_created = models.DateTimeField(auto_now=True, null=True)
 
     def __str__(self):
         return self.name
