1dc383be77abeeaf2c379c2d18d57f9d09edc808
dimat <dimaprizma2016@gmail.com>
2021-01-20 09:54:06 +0200

    Homework 12. Django-debug-toolbar, django-silk. - corrected order of imports

--- django_example_project/urls.py
+++ django_example_project/urls.py
@@ -13,8 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import debug_toolbar
 from catalog import views
+import debug_toolbar
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -53,4 +53,3 @@ if settings.DEBUG:
         path('__debug__', include(debug_toolbar.urls)),  # Add Debug Tollbar's URLconf
         path('silk/', include('silk.urls', namespace='silk')),  # Add Silk's URLconf
     ]
-
