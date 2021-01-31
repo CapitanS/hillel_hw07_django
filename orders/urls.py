6f364c3231a64bb9892e63363c9c23b74a4f1a4f
dimat <dimaprizma2016@gmail.com>
2021-01-31 10:13:15 +0200

    Homework 15. Class based views and pagination - added Views in views.py - corrected templates

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='products/', permanent=True)),
     path('products/', views.ProductListView.as_view(), name='products'),
     path('product/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
     path('product/create/', views.ProductCreate.as_view(), name='product-create'),
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views import generic
+from django.views.generic import CreateView, DeleteView, UpdateView
+from orders.models import Product
+
 
 # Create your views here.
+class ProductListView(generic.ListView):
+    """Generic class-based view for a list of author."""
+    model = Product
+    paginate_by = 2
+
+
+class ProductDetailView(generic.DetailView):
+    """Generic class-based view for a product."""
+    model = Product
+
+
+class ProductCreate(LoginRequiredMixin, CreateView):
+    model = Product
+    fields = ['name', 'price']
+
+
+class ProductUpdate(LoginRequiredMixin, UpdateView):
+    model = Product
+    fields = '__all__'  # Not recommended (potential security issue if more fields added)
+
+
+class ProductDelete(LoginRequiredMixin, DeleteView):
+    model = Product
+    success_url = reverse_lazy('products')
